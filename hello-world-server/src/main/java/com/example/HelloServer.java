382cc98c1a6ae62994797e5f472d97986992587e
Amol Sontakke <amolsontakke96@gmail.com>
2025-08-25 08:17:52 +0000

    testing

--- hello-world-server/src/main/java/com/example/HelloServer.java
+++ hello-world-server/src/main/java/com/example/HelloServer.java
@@ -21,7 +21,7 @@ public class HelloServer {
     static class HelloHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            String response = "Hello, Amol Welcom to java";
+            String response = "Hello, Amol Welcom to java Server";
             exchange.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
